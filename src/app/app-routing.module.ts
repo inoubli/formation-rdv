97670e1d785159cc0347afbcdfdfe5a9584a93c6
khaldounbhy <khaldoun.benhadjyahia@esprit.tn>
2020-07-08 17:34:16 +0200

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MesRdvComponent } from './mes-rdv/mes-rdv.component';
+import { PraticienComponent } from './praticien/praticien.component';
+import { ListParticienComponent } from './list-particien/list-particien.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'mes-rendez-vous', component: MesRdvComponent }, {
+    path: "praticien", component: ListParticienComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
