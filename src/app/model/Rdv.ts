df6b114d78ed63f6d2698c6560b237308abb3968
alexmao <alexmao@56kb.ovh>
2020-07-08 16:24:17 +0200

    Mise à jour modele de données

--- src/app/model/Rdv.ts
+++ src/app/model/Rdv.ts
@@ -1,6 +1,6 @@
 export class Rdv {
-  constructor(public id: number, public idPatient?: number, public idParticien?: number, public motif?: string, public dateRdv?: Date,
-              public confirmer?: boolean) {
+  constructor(public id: number, public idPatient?: number, public idPraticien?: number, public motif?: string,
+              public dateRdv?: Date, public confirmer?: boolean) {
   }
 
 }
