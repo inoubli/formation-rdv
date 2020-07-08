3b82fbdf1eb05e49e66ce4bb866a4f4aae90939f
inoubli <aymen.inoubli@gmail.com>
2020-07-08 12:49:01 +0200

    Generated Project Components menu/login/mesRdv/praticien/rdv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PraticienComponent } from './praticien/praticien.component';
+import { RdvComponent } from './rdv/rdv.component';
+import { MesRdvComponent } from './mes-rdv/mes-rdv.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PraticienComponent,
+    RdvComponent,
+    MesRdvComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mes-rdv/mes-rdv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mes-rdv',
+  templateUrl: './mes-rdv.component.html',
+  styleUrls: ['./mes-rdv.component.css']
+})
+export class MesRdvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
